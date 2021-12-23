2e24069f4aa51444be460b44bbaf4360edde8859
Luca <mylince@gmail.com>
2021-12-23 20:54:50 +0100

    Christmas 4 eva test

--- js/christmas.js
+++ js/christmas.js
@@ -1,3 +1,14 @@
+$('.button').on('click', function(){
+
+    if (window.navigator.vibrate) {
+        window.navigator.vibrate(50);
+    }
+
+    var button = $(this);
+    button.parent().fadeOut(1000);
+
+});
+
 document.addEventListener('DOMContentLoaded', function(){
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
